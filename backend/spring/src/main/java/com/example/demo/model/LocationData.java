4a0ba133f33e3549ac5ebb87ef63d41f8dbabde2
kolo <kolo@kakao.com>
2024-03-11 17:10:24 +0900

    feat: DB연결,근처 가게 조회

---
+++ backend/spring/src/main/java/com/example/demo/model/LocationData.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+public class LocationData {
+    private double latitude;
+    private double longitude;
+
+    public LocationData() {}
+
+    public double getLatitude() {return latitude;}
+
+    public void setLatitude(double latitude) {this.latitude = latitude;}
+
+    public double getLongitude() {return longitude;}
+
+    public void setLongitude(double longitude) {this.longitude = longitude;}
+}
