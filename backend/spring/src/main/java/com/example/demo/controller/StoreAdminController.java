ad3e86d804d944e75e37a3541be034aeb40c8ca5
kolo <kolo@kakao.com>
2024-03-16 02:07:13 +0900

    feat : 점주별 DestinationVariable 적용

--- backend/spring/src/main/java/com/example/demo/controller/StoreAdminController.java
+++ backend/spring/src/main/java/com/example/demo/controller/StoreAdminController.java
@@ -1,36 +1,37 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
 import com.example.demo.DTO.ToClient.AdminLoginResponse;
 import com.example.demo.DTO.ToServer.AdminLoginRequest;
 import com.example.demo.model.DataBase.Admin;
 import com.example.demo.service.AdminLoginService;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.util.Date;
 import io.jsonwebtoken.security.Keys;
+
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-import java.util.Base64;
+import java.util.Date;
 
 @Controller
 public class StoreAdminController {
+
     @Autowired
     private AdminLoginService adminLoginService; // AdminLoginService 주입
 
-    @MessageMapping("/admin/StoreAdmin/login")
-    @SendTo("/topic/admin/StoreAdmin/login")
-    public AdminLoginResponse login(AdminLoginRequest request) {
+    @MessageMapping("/admin/StoreAdmin/login/{adminPhoneNumber}")
+    @SendTo("/topic/admin/StoreAdmin/login/{adminPhoneNumber}")
+    public AdminLoginResponse login(AdminLoginRequest request, @DestinationVariable String adminPhoneNumber) {
         // 전화번호와 비밀번호를 받아 서비스를 통해 인증
         Admin isValidUser = adminLoginService.validateAdminCredentials(request.getAdminPhoneNumber(), request.getAdminPassword());
-
         if (isValidUser != null) {
             // JWT 발급
-            String token = generateJwtToken(request.getAdminPhoneNumber());
+            String token = generateJwtToken(adminPhoneNumber);
             // 인증된 사용자의 storeCode를 가져오는 로직 (가정)
             return new AdminLoginResponse("success", token, isValidUser.getAdminStoreCode());
         } else {
@@ -38,6 +39,7 @@ public class StoreAdminController {
             return new AdminLoginResponse("failure", null, 0);
         }
     }
+
     // JWT 토큰 생성 로직
     private String generateJwtToken(String adminPhoneNumber) {
         long expirationTime = 86400000; // 24시간의 밀리초
