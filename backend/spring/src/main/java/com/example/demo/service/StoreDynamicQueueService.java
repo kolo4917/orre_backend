883867afb23e894501f3b950a1d0710333f9cc3b
kolo <kolo@kakao.com>
2024-03-16 02:56:31 +0900

    feat : 점주 실시간 웨이팅 정보 확인가능

--- backend/spring/src/main/java/com/example/demo/service/StoreDynamicQueueService.java
+++ backend/spring/src/main/java/com/example/demo/service/StoreDynamicQueueService.java
@@ -16,18 +16,18 @@ public class StoreDynamicQueueService {
     public StoreDynamicQueueService(StoreDynamicQueueRepository storeDynamicQueueRepository) {
         this.storeDynamicQueueRepository = storeDynamicQueueRepository;
     }
-    public List<StoreDynamicQueue> findStoreDynamicQueue() {
+    public List<StoreDynamicQueue> findStoreDynamicQueue(Integer storeCode) {
         List<StoreDynamicQueue> dynamicQueues = new ArrayList<>();
         // 레포지토리를 통해 가게의 동적 대기열 정보를 조회
-        List<Object[]> dynamicQueueData = storeDynamicQueueRepository.findStoreDynamicQueue();
+        List<Object[]> dynamicQueueData = storeDynamicQueueRepository.findStoreDynamicQueue(storeCode);
 
         // 조회된 데이터를 StoreDynamicQueue 객체로 변환하여 리스트에 추가
         for (Object[] data : dynamicQueueData) {
-            int storeCode = (int) data[0];
+            int storeCode2 = (int) data[0];
             int numberOfTeamsWaiting = (int) data[1];
             int estimatedWaitingTime = 5 * numberOfTeamsWaiting; // 시간 계산 알고리즘 추가 필요
 
-            StoreDynamicQueue dynamicQueue = new StoreDynamicQueue(storeCode, numberOfTeamsWaiting, estimatedWaitingTime);
+            StoreDynamicQueue dynamicQueue = new StoreDynamicQueue(storeCode2, numberOfTeamsWaiting, estimatedWaitingTime);
             dynamicQueues.add(dynamicQueue);
         }
 
