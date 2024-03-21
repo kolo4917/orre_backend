94e35c77677a2076a7155a5dd965f1c81373dbdc
kolo <kolo@kakao.com>
2024-03-21 19:20:11 +0900

    refactor : 동적 대기열 리턴 형태 변경

--- backend/spring/src/main/java/com/example/demo/service/StoreDynamicQueueService.java
+++ backend/spring/src/main/java/com/example/demo/service/StoreDynamicQueueService.java
@@ -17,41 +17,39 @@ public class StoreDynamicQueueService {
         this.storeDynamicQueueRepository = storeDynamicQueueRepository;
     }
 
-    public List<StoreDynamicQueue> findStoreDynamicQueue(Integer storeCode) {
-        List<StoreDynamicQueue> dynamicQueues = new ArrayList<>();
+    public StoreDynamicQueue findStoreDynamicQueue(Integer storeCode) {
         // 레포지토리를 통해 가게의 동적 대기열 정보를 조회
         List<Object[]> dynamicQueueData = storeDynamicQueueRepository.findStoreDynamicQueue(storeCode);
 
-        // 데이터가 비어 있는지 확인
-        if (dynamicQueueData.isEmpty()) {
-            // 처리할 데이터가 없으므로 빈 리스트를 반환
-            return dynamicQueues;
-        }
-
         // 대기 팀 리스트를 초기화합니다.
         List<Integer> waitingTeamList = new ArrayList<>();
+        List<Integer> enteringTeamList = new ArrayList<>();
+
+        // 데이터가 비어 있는지 확인
+        if (dynamicQueueData.isEmpty()) {
+            // 데이터가 비어 있으면 storeCode만 반환하고 나머지 값들은 null로 설정
+            return new StoreDynamicQueue(storeCode, waitingTeamList, enteringTeamList, 5);
+        }
 
         // 조회된 데이터를 기반으로 대기 팀 리스트를 채웁니다.
         for (Object[] data : dynamicQueueData) {
-            waitingTeamList.add((int) data[1]);
+            waitingTeamList.add((Integer) data[1]);
         }
 
-        // 상위 3개 팀을 입장 팀 리스트에 추가합니다. // 가게가 부르는 매커니즘으로 해도 좋을 듯
-        List<Integer> enteringTeamList = new ArrayList<>();
+        // 상위 3개 팀을 입장 팀 리스트에 추가합니다.
         for (int i = 0; i < Math.min(waitingTeamList.size(), 3); i++) {
             enteringTeamList.add(waitingTeamList.get(i));
         }
 
         // 조회된 데이터의 첫 번째 데이터를 사용하여 가게 코드를 가져옵니다.(한번만 가져와도 됨)
-        int dynamicstoreCode = (int) dynamicQueueData.get(0)[0];
+        int dynamicstoreCode = (Integer) dynamicQueueData.get(0)[0];
 
         // 예상 대기 시간을 계산
         int estimatedWaitingTimePerTeam = 5; // 대기 시간 (팀당) //ML로 통계 쌓이면 구현 예정
 
         // StoreDynamicQueue 객체를 생성하고 반환합니다.
         StoreDynamicQueue dynamicQueue = new StoreDynamicQueue(dynamicstoreCode, waitingTeamList, enteringTeamList, estimatedWaitingTimePerTeam);
-        dynamicQueues.add(dynamicQueue);
 
-        return dynamicQueues;
+        return dynamicQueue;
     }
 }
