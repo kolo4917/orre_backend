e6a000d12a519e8f265f89c9b67ed82fe35b2e08
kolo <kolo@kakao.com>
2024-04-07 00:28:09 +0900

    feat : admin Table Unlock

--- backend/spring/src/main/java/com/example/demo/service/TableLockingService.java
+++ backend/spring/src/main/java/com/example/demo/service/TableLockingService.java
@@ -21,19 +21,19 @@ public class TableLockingService {
     }
 
     @Transactional // 테이블 이용 상태 변화
-    public boolean unlockTable(Integer storeCode, Integer tableNumber, Integer waitingNumber) {
+    public String unlockTable(Integer storeCode, Integer tableNumber, Integer waitingNumber) {
         // 조건에 맞는 user_store_wait 레코드 찾기
         UserStoreWait userStoreWait = userStoreWaitRepository.findByStoreCodeAndWaitingNumber(storeCode, waitingNumber);
         if (userStoreWait != null) {
             // 해당 가게 코드와 테이블 번호로 restaurant_table 레코드 찾기
             RestaurantTable table = tableLockingRepository.findByStoreCodeAndTableNumber(storeCode, tableNumber);
-            if (table != null && table.getTableAvailable() != 1) {
+            if ((table != null) && (table.getTableAvailable() != 1)) {
                 table.setTableAvailable(1); // 테이블 사용 가능으로 변경
-                table.setTablePhoneNumber(userStoreWait.getPhoneNumber()); // 전화번호로 인원 수 설정(가정)
+                table.setTablePhoneNumber(userStoreWait.getPhoneNumber()); // 전화번호 할당
                 tableLockingRepository.save(table); // 변경 사항 저장
-                return true; // 테이블 언락 성공
+                return userStoreWait.getPhoneNumber(); // 전화번호 반환
             }
         }
-        return false; // 해당하는 테이블 또는 대기 번호를 찾지 못한 경우
+        return null; // 언락 실패 또는 전화번호 없음
     }
 }
