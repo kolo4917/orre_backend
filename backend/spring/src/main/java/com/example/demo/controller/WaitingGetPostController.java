278a47f3b8b4f21ddad929d86cdd10bb55fbd8e6
kolo <kolo@kakao.com>
2024-04-16 13:23:02 +0900

    feat : user basicstore get

--- backend/spring/src/main/java/com/example/demo/controller/WaitingGetPostController.java
+++ backend/spring/src/main/java/com/example/demo/controller/WaitingGetPostController.java
@@ -3,23 +3,37 @@ package com.example.demo.controller;
 import com.example.demo.DTO.ToClient.StoreDTO;
 import com.example.demo.DTO.ToServer.StoreInfoRequest;
 import com.example.demo.service.StoreService;
+import com.example.demo.service.WaitingService;
+import com.example.demo.DTO.ToClient.StoreListDTO;
+import com.example.demo.model.LocationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 public class WaitingGetPostController {
 
     private final StoreService storeService;
+    private final WaitingService waitingService;
 
     @Autowired
-    public WaitingGetPostController(StoreService storeService) {
+    public WaitingGetPostController(StoreService storeService, WaitingService waitingService) {
         this.storeService = storeService;
+        this.waitingService = waitingService;
     }
 
     @PostMapping("/api/user/storeInfo")
     public StoreDTO getStoreInfo(@RequestBody StoreInfoRequest request) {
         return storeService.getStoreDetailsByStoreCode(request.getStoreCode());
+    }
+    @GetMapping("/api/user/storeList/basicStores")
+    public List<StoreListDTO> getBasicStores(@RequestBody LocationData locationData) {
+        List<StoreListDTO> basicStores = waitingService.findBasicStore(locationData);
+        return basicStores;
     }
 }
