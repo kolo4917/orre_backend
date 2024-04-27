b15ec30af966040612e2410c531b6a87f9a4651c
kolo <kolo@kakao.com>
2024-04-28 01:11:39 +0900

    fix : get method

--- backend/spring/src/main/java/com/example/demo/controller/WaitingGetPostController.java
+++ backend/spring/src/main/java/com/example/demo/controller/WaitingGetPostController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,13 +32,25 @@ public class WaitingGetPostController {
     public StoreDTO getStoreInfo(@RequestBody StoreInfoRequest request) {
         return storeService.getStoreDetailsByStoreCode(request.getStoreCode());
     }
-    @GetMapping("/api/user/storeList/basicStores")
+    /*@GetMapping("/api/user/storeList/basicStores")
     public List<StoreListDTO> getBasicStores(@RequestBody LocationData locationData) {
         List<StoreListDTO> basicStores = waitingService.findBasicStore(locationData);
         return basicStores;
+    }*/
+    @GetMapping("/api/user/storeList/basicStores")
+    public List<StoreListDTO> getBasicStores(@RequestParam(name = "latitude") double latitude, @RequestParam(name = "longitude") double longitude){
+        LocationData locationData = new LocationData(latitude, longitude);
+        List<StoreListDTO> basicStores = waitingService.findBasicStore(locationData);
+        return basicStores;
     }
-    @GetMapping("/api/user/storeList/nearestStores")
+    /*@GetMapping("/api/user/storeList/nearestStores")
     public List<StoreListDTO> getNearestStores(@RequestBody LocationData locationData) {
+        List<StoreListDTO> nearestStores = waitingService.findNearestStores(locationData);
+        return nearestStores;
+    }*/
+    @GetMapping("/api/user/storeList/nearestStores")
+    public List<StoreListDTO> getNearestStores(@RequestParam(name = "latitude") double latitude, @RequestParam(name = "longitude") double longitude){
+        LocationData locationData = new LocationData(latitude, longitude);
         List<StoreListDTO> nearestStores = waitingService.findNearestStores(locationData);
         return nearestStores;
     }
