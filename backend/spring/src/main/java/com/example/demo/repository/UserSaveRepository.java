b66054b0b5968868952fac6fee6e571fcee3df55
kolo <kolo@kakao.com>
2024-05-02 02:20:10 +0900

    feat : user signup 중복 방지

--- backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSaveRepository extends JpaRepository<User, String> {
     boolean existsByToken(String token);
+    boolean existsByPhoneNumber(String phoneNumber);
+
 }
