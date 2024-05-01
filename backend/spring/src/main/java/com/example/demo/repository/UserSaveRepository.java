4d3e46fd803770f082bc3eab081a8518fe09ff5b
kolo <kolo@kakao.com>
2024-05-02 02:06:43 +0900

    feat : user signup 난수 토큰

--- backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.model.DataBase.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSaveRepository extends JpaRepository<User, String> {
-    // phoneNumber를 기본 키로 사용하기 때문에, ID의 타입은 String입니다.
+    boolean existsByToken(String token);
 }
