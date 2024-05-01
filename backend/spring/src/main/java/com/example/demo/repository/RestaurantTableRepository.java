fe113c6e39128a5691845dd54fc748352340674e
kolo <kolo@kakao.com>
2024-05-01 12:50:27 +0900

    feat : admin table remove

--- backend/spring/src/main/java/com/example/demo/repository/RestaurantTableRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/RestaurantTableRepository.java
@@ -8,6 +8,13 @@ import java.util.List;
 
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Integer> {
 
+    //@Query 어노테이션을 사용한 사용자 정의 쿼리:
     @Query(value = "SELECT rt.tableNumber, rt.tablePersonNumber, rt.tableAvailable FROM RestaurantTable rt WHERE rt.storeCode = :storeCode")
     List<Object[]> findAvailableTablesByStoreCode(@Param("storeCode") Integer storeCode);
+
+    //메서드 이름으로 쿼리 생성:
+    RestaurantTable findByStoreCodeAndTableNumber(Integer storeCode, Integer tableNumber);
+
+
+
 }
