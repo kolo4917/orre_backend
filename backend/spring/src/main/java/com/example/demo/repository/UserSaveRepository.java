bde7fe24111ece91e1c3e943d9ac7e43b54fd967
kolo <kolo@kakao.com>
2024-05-02 19:14:15 +0900

    feat, fix : user signup remove, user login dto

--- backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserSaveRepository extends JpaRepository<User, String> {
     boolean existsByToken(String token);
     boolean existsByPhoneNumber(String phoneNumber);
+    void deleteByPhoneNumber(String phoneNumber);
+    User findByPhoneNumber(String phoneNumber);
+
+
 
 }
