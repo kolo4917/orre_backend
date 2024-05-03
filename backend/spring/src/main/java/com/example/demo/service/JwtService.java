ce678580d9d0a96456eaf9a93b5e8e37e718805d
kolo <kolo@kakao.com>
2024-05-03 13:55:39 +0900

    feat : user menu order api

--- backend/spring/src/main/java/com/example/demo/service/JwtService.java
+++ backend/spring/src/main/java/com/example/demo/service/JwtService.java
@@ -53,6 +53,29 @@ public class JwtService {
             return false;
         }
     }
+    public boolean isValidUser(String jwtToken) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(jwtToken)
+                    .getBody();
+
+            // JWT 내의 "role" 클레임을 확인하여 값이 "user"인지 확인
+            String role = (String) claims.get("role");
+            if (role != null && role.equals("user")) {
+                // "role"이 "admin"인 경우에만 유효하도록 반환
+                return true;
+            }
+
+            // "role"이 "user"이 아닌 경우 유효하지 않음
+            return false;
+        } catch (Exception e) {
+            // 예외 발생 시 로그를 출력
+            logger.error("JWT validation error", e);
+            return false;
+        }
+    }
     public boolean revokeUserJWT(String phoneNumber) {
         try {
             Date now = new Date();
