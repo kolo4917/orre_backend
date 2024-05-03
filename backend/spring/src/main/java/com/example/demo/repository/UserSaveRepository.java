aeac1a7e1ea15f9f530cb56fa6dc3277d54d3725
kolo <kolo@kakao.com>
2024-05-03 17:37:53 +0900

    feat : user reset password

--- backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/UserSaveRepository.java
@@ -2,12 +2,20 @@ package com.example.demo.repository;
 
 import com.example.demo.model.DataBase.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserSaveRepository extends JpaRepository<User, String> {
     boolean existsByToken(String token);
     boolean existsByPhoneNumber(String phoneNumber);
     void deleteByPhoneNumber(String phoneNumber);
     User findByPhoneNumber(String phoneNumber);
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.phoneNumber = :phoneNumber")
+    void updateUserPassword(@Param("phoneNumber") String phoneNumber, @Param("newPassword") String newPassword);
+
 
 
 
