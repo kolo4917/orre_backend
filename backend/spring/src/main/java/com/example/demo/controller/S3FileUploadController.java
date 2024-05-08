aa4896411458361e0deb5a6dfc8645043881b24c
kolo <kolo@kakao.com>
2024-05-08 14:46:42 +0900

    feat : admin menu remove using s3

--- backend/spring/src/main/java/com/example/demo/controller/S3FileUploadController.java
+++ backend/spring/src/main/java/com/example/demo/controller/S3FileUploadController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.DTO.ToClient.BooleanResponse;
 import com.example.demo.DTO.ToClient.StatusResponse;
 import com.example.demo.DTO.ToServer.S3UploadRequest;
+import com.example.demo.DTO.ToServer.S3RemoveRequest;
 import com.example.demo.service.S3FileUploadService;
+import com.example.demo.service.JwtService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,23 +20,52 @@ import java.io.IOException;
 public class S3FileUploadController {
 
     private final S3FileUploadService s3FileUploadService;
+    private final JwtService jwtService;
 
-    public S3FileUploadController(S3FileUploadService s3FileUploadService) {
+    public S3FileUploadController(S3FileUploadService s3FileUploadService, JwtService jwtService) {
         this.s3FileUploadService = s3FileUploadService;
+        this.jwtService = jwtService;
     }
 
-    @PostMapping("/api/admin/StoreAdmin/menu/upload")
+    @PostMapping("/api/admin/StoreAdmin/menu/s3/upload")
     public StatusResponse uploadFile(@RequestPart(name = "file") MultipartFile file,
                                  @RequestPart (name = "request") S3UploadRequest request) {
         int storeCode = request.getStoreCode();
         String singleMenuCode = request.getSingleMenuCode();
-        String fileUrl = s3FileUploadService.uploadFile(file, storeCode, singleMenuCode);
+        String jwtAdmin = request.getJwtAdmin();
 
-        if(!fileUrl.equals("1201")){
-            String uploadToDatabase = s3FileUploadService.uploadToDatabase(request, fileUrl);
-            return new StatusResponse(uploadToDatabase);
+        boolean isValidAdmin = jwtService.isValid(jwtAdmin);
+        if (isValidAdmin) {
+            String fileUrl = s3FileUploadService.uploadFile(file, storeCode, singleMenuCode);
+            if(!fileUrl.equals("5001")){
+                String uploadToDatabase = s3FileUploadService.uploadToDatabase(request, fileUrl);
+                return new StatusResponse(uploadToDatabase);
+            }
+            return new StatusResponse("5002");
         }
-        return new StatusResponse("1201");
+        else {
+            return new StatusResponse("5001");
+        }
+
     }
+    @PostMapping("/api/admin/StoreAdmin/menu/s3/remove")
+    public StatusResponse removeFile(@RequestBody S3RemoveRequest request){
+
+        String jwtAdmin = request.getJwtAdmin();
+        int storeCode = request.getStoreCode();
+        String menu = request.getMenu();
+        String menuCode = request.getMenuCode();
+
+        boolean isValidAdmin = jwtService.isValid(jwtAdmin);
+        if (isValidAdmin) {
+            String status = s3FileUploadService.removeToDatabase(storeCode, menu, menuCode);
+            return new StatusResponse(status);
+            }
+        else {
+            return new StatusResponse("5001");
+        }
+
+    }
+
 
 }
