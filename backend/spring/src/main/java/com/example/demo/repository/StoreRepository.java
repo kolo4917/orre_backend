b799fa2658a99255b7ad56ef18245808b59ab04f
kolo <kolo@kakao.com>
2024-05-08 15:31:57 +0900

    feat : admin store waiting available update

--- backend/spring/src/main/java/com/example/demo/repository/StoreRepository.java
+++ backend/spring/src/main/java/com/example/demo/repository/StoreRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.DataBase.MenuInfo;
 import com.example.demo.model.DataBase.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface StoreRepository extends JpaRepository<Store, Integer> {
     @Query("SELECT s.storeCode, s.storeName, s.storePhoneNumber, s.storeWaitingAmount, s.storeWaitingAvailable, s.storeInfoVersion, s.storeImageMain, s.storeIntroduce, s.storeCategory, s.openingTime, s.closingTime, s.lastOrderTime, s.startBreakTime, s.endBreakTime FROM Store s WHERE s.storeCode = :storeCode")
     Object[] findStoreDetailsByStoreCode(@Param("storeCode") Integer storeCode);
+
+    Store findByStoreCode(Integer storeCode);
+
 }
