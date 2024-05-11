7b042fc8ce9b55f5f2db285845a1dfe72aab1d65
kolo <kolo@kakao.com>
2024-05-11 14:39:53 +0900

    feat : admin order Alarm

--- backend/spring/src/main/java/com/example/demo/service/StoreMenuOrderService.java
+++ backend/spring/src/main/java/com/example/demo/service/StoreMenuOrderService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.DTO.ToServer.StoreMenuOrderRequest;
+import com.example.demo.config.events.EventPublisherService;
+import com.example.demo.config.events.StoreMenuOrderEvent;
 import com.example.demo.repository.MenuInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoreMenuOrderService {
 
     private final MenuInfoRepository menuInfoRepository;
-
     @Autowired
-    public StoreMenuOrderService(MenuInfoRepository menuInfoRepository) {
+    private EventPublisherService eventPublisherService;
+    @Autowired
+    public StoreMenuOrderService(MenuInfoRepository menuInfoRepository, EventPublisherService eventPublisherService) {
         this.menuInfoRepository = menuInfoRepository;
+        this.eventPublisherService = eventPublisherService;
     }
+
+
 
     @Transactional
     public void ModifyMenuAmount(StoreMenuOrderRequest request) {
@@ -25,6 +31,8 @@ public class StoreMenuOrderService {
 
         // MenuInfo 엔터티를 업데이트하는 쿼리 실행
         menuInfoRepository.updateAmountByStoreCodeAndTableNumberAndMenuCode(storeCode, tableNumber, menuCode, amountToAdd);
+        eventPublisherService.publishStoreOrder(new StoreMenuOrderEvent(this, request),1000);
+
     }
     @Transactional
     public void ModifyMenuAmountForUser(StoreMenuOrderRequest request) {
@@ -36,6 +44,8 @@ public class StoreMenuOrderService {
             throw new IllegalArgumentException("Amount to add must be greater than 0");
         }
         menuInfoRepository.updateAmountByStoreCodeAndTableNumberAndMenuCode(storeCode, tableNumber, menuCode, amountToAdd);
+        eventPublisherService.publishStoreOrder(new StoreMenuOrderEvent(this, request),1000);
+
 
     }
 }
