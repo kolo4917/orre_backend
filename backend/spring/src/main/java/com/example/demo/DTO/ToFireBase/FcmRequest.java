7da9492efb668f67578c4ae2564ae911753dca5e
kolo <kolo@kakao.com>
2024-06-07 17:34:22 +0900

    fix : fcm sound error

--- backend/spring/src/main/java/com/example/demo/DTO/ToFireBase/FcmRequest.java
+++ backend/spring/src/main/java/com/example/demo/DTO/ToFireBase/FcmRequest.java
@@ -21,13 +21,17 @@ public class FcmRequest {
     public static class Message {
         private String token;
         private Notification notification;
+        private Android android;
+        private Apns apns;
 
         public Message() {
         }
 
-        public Message(String token, Notification notification) {
+        public Message(String token, Notification notification, Android android, Apns apns) {
             this.token = token;
             this.notification = notification;
+            this.android = android;
+            this.apns = apns;
         }
 
         public String getToken() {
@@ -45,20 +49,34 @@ public class FcmRequest {
         public void setNotification(Notification notification) {
             this.notification = notification;
         }
+
+        public Android getAndroid() {
+            return android;
+        }
+
+        public void setAndroid(Android android) {
+            this.android = android;
+        }
+
+        public Apns getApns() {
+            return apns;
+        }
+
+        public void setApns(Apns apns) {
+            this.apns = apns;
+        }
     }
 
     public static class Notification {
         private String title;
         private String body;
-        private String sound; // sound 필드 추가
 
         public Notification() {
         }
 
-        public Notification(String title, String body, String sound) { // 생성자 수정
+        public Notification(String title, String body) {
             this.title = title;
             this.body = body;
-            this.sound = sound;
         }
 
         public String getTitle() {
@@ -76,13 +94,100 @@ public class FcmRequest {
         public void setBody(String body) {
             this.body = body;
         }
+    }
 
-        public String getSound() { // getter 추가
-            return sound;
+    public static class Android {
+        private AndroidNotification notification;
+
+        public Android() {
         }
 
-        public void setSound(String sound) { // setter 추가
-            this.sound = sound;
+        public Android(AndroidNotification notification) {
+            this.notification = notification;
+        }
+
+        public AndroidNotification getNotification() {
+            return notification;
+        }
+
+        public void setNotification(AndroidNotification notification) {
+            this.notification = notification;
+        }
+
+        public static class AndroidNotification {
+            private String sound;
+
+            public AndroidNotification() {
+            }
+
+            public AndroidNotification(String sound) {
+                this.sound = sound;
+            }
+
+            public String getSound() {
+                return sound;
+            }
+
+            public void setSound(String sound) {
+                this.sound = sound;
+            }
+        }
+    }
+
+    public static class Apns {
+        private ApnsPayload payload;
+
+        public Apns() {
+        }
+
+        public Apns(ApnsPayload payload) {
+            this.payload = payload;
+        }
+
+        public ApnsPayload getPayload() {
+            return payload;
+        }
+
+        public void setPayload(ApnsPayload payload) {
+            this.payload = payload;
+        }
+
+        public static class ApnsPayload {
+            private Aps aps;
+
+            public ApnsPayload() {
+            }
+
+            public ApnsPayload(Aps aps) {
+                this.aps = aps;
+            }
+
+            public Aps getAps() {
+                return aps;
+            }
+
+            public void setAps(Aps aps) {
+                this.aps = aps;
+            }
+
+            public static class Aps {
+                private String sound;
+
+                public Aps() {
+                }
+
+                public Aps(String sound) {
+                    this.sound = sound;
+                }
+
+                public String getSound() {
+                    return sound;
+                }
+
+                public void setSound(String sound) {
+                    this.sound = sound;
+                }
+            }
         }
     }
 }
